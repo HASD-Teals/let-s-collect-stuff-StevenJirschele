1e81764a60f307edb1eaf71d26b652cb299c7fd4
StevenJirschele <bmaxx321@gmail.com>
2022-02-24 10:05:17 -0600

    Progress

--- lets-collect-stuff/src/CoinClient.java
+++ lets-collect-stuff/src/CoinClient.java
@@ -20,5 +20,53 @@ public class CoinClient {
         myList.remove(test);
         System.out.println("\n");
         System.out.println(myList);
+
+        System.out.println("\n");
+
+        System.out.println("\n");
+        System.out.println(searchList(myList, "color", "silver"));
+    }
+    public static void addItem(List<Coin> myList, Coin add) {
+        myList.add(add);
+    }
+    public static void removeItem(List<Coin> myList, Coin remove) {
+        myList.remove(remove);
+    }
+    public static List<Coin> searchList(List<Coin> myList, String prop, String context) {
+        List<Coin> temp = new ArrayList<Coin>();
+        if(prop.toLowerCase().equals("color")) {
+            for(Coin i:myList) {
+                if(i.getColor().toLowerCase().equals(context.toLowerCase())) {
+                    temp.add(i);
+                }
+            }
+        }
+        else if(prop.equals("material")) {
+            for(Coin i:myList) {
+                if(i.getMaterial().toLowerCase().equals(context.toLowerCase())) {
+                    temp.add(i);
+                }
+            }
+        }
+        else if(prop.equals("person")) {
+            for(Coin i:myList) {
+                if(i.getPerson().toLowerCase().equals(context.toLowerCase())) {
+                    temp.add(i);
+                }
+            }
+        }
+        else if(prop.equals("value")) {
+            for(Coin i:myList) {
+                if(i.getValue() == Double.parseDouble(context)) {
+                    temp.add(i);
+                }
+            }
+        } else {
+            return null;
+        }
+        return temp;
+    }
+    public static int listCount(List<Coin> myList) {
+        return myList.size();
     }
 }
